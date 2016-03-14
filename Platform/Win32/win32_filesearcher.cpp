379b6cdfe806149987b6b6c77ee307368a4a1145
Andrew Learn <chcniz@gmail.com>
2016-03-14 17:43:44 -0400

    win32 fixes

--- Platform/Win32/win32_filesearcher.cpp
+++ Platform/Win32/win32_filesearcher.cpp
@@ -6,20 +6,21 @@ FileSearcher::FileSearcher(const char *path, FileSearcherCB callback, void *para
 	mp_params = params;
 }
 void FileSearcher::run(const char *path) {
-	const char *the_path = (const char *)&m_path;
+	const char *_path = (const char *)&m_path;
 	if(path) {
-		the_path = path;
+		_path = path;
 	}
+	FileInfo file_info;
    	WIN32_FIND_DATA FindFileData;
    	HANDLE hFind;
    	char the_path[MAX_PATH+256];
    	sprintf(the_path,"%s\\*");
-   	hFind = FindFirstFile(the_path,&FindFirstFile);
+   	hFind = FindFirstFile(the_path,&FindFileData);
 	do {
-		file_info.name = (char *)&FindFileData.cFileName,;
+		file_info.name = (char *)&FindFileData.cFileName;
 		if(mp_cb_onfound && ~FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 			mp_cb_onfound(&file_info, mp_params);
-		} else if(file_info.name != '.') { //skip cur dir, hidden dirs, etc
+		} else if(file_info.name[0] != '.') { //skip cur dir, hidden dirs, etc
 				run(file_info.name);
 			}
 	} while(FindNextFile(hFind, &FindFileData) != 0);
